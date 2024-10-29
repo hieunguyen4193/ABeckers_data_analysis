839570f2765dd785cdc23028497e9f5c2e7aba3d
trnguyenuka <trnguyenuka@github.com>
2024-10-29 09:46:03 +0100

    update

--- run_pipeline_LV20_21_PV20_21.R
+++ run_pipeline_LV20_21_PV20_21.R
@@ -16,108 +16,112 @@ source(file.path(path.to.pipeline.src, "scRNA_GEX_pipeline.R"))
 path.to.storage <- "/media/hieunguyen/HD01/storage"
 path2input <- file.path(path.to.storage, PROJECT, batch.id)
 outdir <- "/home/hieunguyen/CRC1382/outdir"
+
 # output.version <- "default"
-output.version <- "v0.1"
+# output.version <- "v0.1"
 
-source("/home/hieunguyen/CRC1382/src_2023/ABeckers/config.R")
-
-path.to.output <- file.path(outdir, PROJECT, batch.id, output.version)
-dir.create(path.to.output, showWarnings = FALSE, recursive = TRUE)
-
-# _____stage lst for single sample_____
-stage_lst <- list()
-
-stage_lst = c(
-  LV20      =   "LV",
-  LV21      =   "LV",
-  PV20      =   "PV",
-  PV21      =   "PV"
-)
-
-MINCELLS  <- 5
-MINGENES  <- 50
-
-save.RDS <- list(s1 = TRUE,
-                 s2 = TRUE,
-                 s3 = TRUE,
-                 s4 = TRUE,
-                 s5 = TRUE,
-                 s6 = TRUE,
-                 s7 = FALSE,
-                 s8 = TRUE,
-                 s8a = TRUE,
-                 s9 = TRUE)
-
-sw <- list(s1 = "on",
-           s2 = "on",
-           s3 = "on",
-           s4 = "on",
-           s5 = "on",
-           s6 = "on",
-           s7 = "off",
-           s8 = "on",
-           s8a = "off",
-           s9 = "on")
-
-rerun <- list(s1 = FALSE, 
-              s2 = FALSE,
-              s3 = FALSE,
-              s4 = FALSE,
-              s5 = FALSE,
-              s6 = FALSE,
-              s7 = FALSE,
-              s8 = FALSE,
-              s8a = FALSE,
-              s9 = FALSE)
-
-
-filter.thresholds <- all.config.params[[output.version]]
-
-print("Using the following configurations:")
-for (i in names(filter.thresholds)){
-  print(sprintf("Param %s: %s", i, filter.thresholds[[i]]))
+for (output.version in c("default", "v0.1")){
+  source("/home/hieunguyen/CRC1382/src_2023/ABeckers/config.R")
+  
+  path.to.output <- file.path(outdir, PROJECT, batch.id, output.version)
+  dir.create(path.to.output, showWarnings = FALSE, recursive = TRUE)
+  
+  # _____stage lst for single sample_____
+  stage_lst <- list()
+  
+  stage_lst = c(
+    LV20      =   "LV",
+    LV21      =   "LV",
+    PV20      =   "PV",
+    PV21      =   "PV"
+  )
+  
+  MINCELLS  <- 5
+  MINGENES  <- 50
+  
+  save.RDS <- list(s1 = TRUE,
+                   s2 = TRUE,
+                   s3 = TRUE,
+                   s4 = TRUE,
+                   s5 = TRUE,
+                   s6 = TRUE,
+                   s7 = FALSE,
+                   s8 = TRUE,
+                   s8a = TRUE,
+                   s9 = TRUE)
+  
+  sw <- list(s1 = "on",
+             s2 = "on",
+             s3 = "on",
+             s4 = "on",
+             s5 = "on",
+             s6 = "on",
+             s7 = "off",
+             s8 = "on",
+             s8a = "off",
+             s9 = "on")
+  
+  rerun <- list(s1 = FALSE, 
+                s2 = FALSE,
+                s3 = FALSE,
+                s4 = FALSE,
+                s5 = FALSE,
+                s6 = FALSE,
+                s7 = FALSE,
+                s8 = FALSE,
+                s8a = FALSE,
+                s9 = FALSE)
+  
+  
+  filter.thresholds <- all.config.params[[output.version]]
+  
+  print("Using the following configurations:")
+  for (i in names(filter.thresholds)){
+    print(sprintf("Param %s: %s", i, filter.thresholds[[i]]))
+  }
+  
+  remove_doublet <- FALSE
+  path.to.10X.doublet.estimation <- "/media/hieunguyen/HD01/storage/DoubletEstimation10X.csv"
+  
+  num.PCA <- 30
+  num.PC.used.in.UMAP <- 30
+  num.PC.used.in.Clustering <- 30
+  
+  num.dim.integration <- 30
+  num.dim.cluster <- 30
+  cluster.resolution <- 0.8
+  
+  filtered.barcodes <- NULL
+  
+  s.obj <- run_pipeline_GEX(path2src=path2src,
+                            path2input=path2input,
+                            path.to.logfile.dir=file.path(path.to.output, "logs"),
+                            stage_lst=stage_lst,
+                            path.to.10X.doublet.estimation=path.to.10X.doublet.estimation,
+                            MINCELLS=MINCELLS,
+                            MINGENES=MINGENES,
+                            PROJECT=PROJECT,
+                            remove_doublet=remove_doublet,
+                            save.RDS=save.RDS,
+                            path.to.output=path.to.output,
+                            rerun=rerun,
+                            DE.test="wilcox",
+                            num.PCA=num.PCA,
+                            num.PC.used.in.UMAP=num.PC.used.in.UMAP,
+                            num.PC.used.in.Clustering=num.PC.used.in.Clustering,
+                            use.sctransform=FALSE,
+                            filtered.barcodes=filtered.barcodes,
+                            filter.thresholds=filter.thresholds,
+                            path.to.anno.contigs=path.to.anno.contigs,
+                            path.to.count.clonaltype=path.to.count.clonaltype,
+                            input.method = "CITESEQ",
+                            my_random_seed = my_random_seed,
+                            cluster.resolution = cluster.resolution, 
+                            num.dim.integration = num.dim.integration,
+                            sw = sw, with.TSNE = TRUE)
+  
+  #### ALWAYS REMEMBER TO SAVE SESSIONINFO !!!!!!
+  writeLines(capture.output(sessionInfo()), file.path(path.to.output, sprintf("%s_sessionInfo.txt", PROJECT)))
 }
 
-remove_doublet <- FALSE
-path.to.10X.doublet.estimation <- "/media/hieunguyen/HD01/storage/DoubletEstimation10X.csv"
-
-num.PCA <- 30
-num.PC.used.in.UMAP <- 30
-num.PC.used.in.Clustering <- 30
-
-num.dim.integration <- 30
-num.dim.cluster <- 30
-cluster.resolution <- 0.8
-
-filtered.barcodes <- NULL
-
-s.obj <- run_pipeline_GEX(path2src=path2src,
-                          path2input=path2input,
-                          path.to.logfile.dir=file.path(path.to.output, "logs"),
-                          stage_lst=stage_lst,
-                          path.to.10X.doublet.estimation=path.to.10X.doublet.estimation,
-                          MINCELLS=MINCELLS,
-                          MINGENES=MINGENES,
-                          PROJECT=PROJECT,
-                          remove_doublet=remove_doublet,
-                          save.RDS=save.RDS,
-                          path.to.output=path.to.output,
-                          rerun=rerun,
-                          DE.test="wilcox",
-                          num.PCA=num.PCA,
-                          num.PC.used.in.UMAP=num.PC.used.in.UMAP,
-                          num.PC.used.in.Clustering=num.PC.used.in.Clustering,
-                          use.sctransform=FALSE,
-                          filtered.barcodes=filtered.barcodes,
-                          filter.thresholds=filter.thresholds,
-                          path.to.anno.contigs=path.to.anno.contigs,
-                          path.to.count.clonaltype=path.to.count.clonaltype,
-                          input.method = "CITESEQ",
-                          my_random_seed = my_random_seed,
-                          cluster.resolution = cluster.resolution, 
-                          num.dim.integration = num.dim.integration,
-                          sw = sw, with.TSNE = TRUE)
-
-#### ALWAYS REMEMBER TO SAVE SESSIONINFO !!!!!!
-writeLines(capture.output(sessionInfo()), file.path(path.to.output, sprintf("%s_sessionInfo.txt", PROJECT)))
 
---
+++ run_to_generate_01_single_samplesR.R
@@ -0,0 +1,35 @@
+gc()
+rm(list = ls())
+
+path.to.project.src <- "/home/hieunguyen/CRC1382/src_2023/ABeckers"
+
+source(file.path(path.to.project.src, "00_import_libraries.R"))
+source(file.path(path.to.project.src, "00_helper_functions.R"))
+
+path.to.src <- "/home/hieunguyen/CRC1382/src_2023/src_pipeline/scRNA_GEX_pipeline/processes_src"
+source(file.path(path.to.src, "s8_integration_and_clustering.R"))
+
+#####----------------------------------------------------------------------#####
+# CONFIGURATIONS AND PREPRATIONS
+#####----------------------------------------------------------------------#####
+PROJECT <- "ABeckers_data_single_samples"
+path.to.storage <- "/media/hieunguyen/HD01/storage"
+outdir <- "/home/hieunguyen/CRC1382/outdir"
+path.to.rmd <- file.path(path.to.project.src, "01_preliminary_analysis.single_sample.Rmd")
+
+for (output.version in c("v0.1", "default")){
+  for (sample.id in c("LV20", "PV20", "LV21", "PV21", "LV73", "PV73")){
+    save.html.name <- str_replace(basename(path.to.rmd), ".Rmd", sprintf(".%s.html", sample.id))
+    path.to.save.html <- file.path(outdir, PROJECT, sample.id, "html_outputs", output.version)
+    dir.create(path.to.save.html, showWarnings = FALSE, recursive = TRUE)
+    
+    rmarkdown::render(input = path.to.rmd, 
+                      params = list(
+                        sample.id = sample.id,
+                        output.version = output.version
+                      ),
+                      output_file = save.html.name, 
+                      output_dir = path.to.save.html)
+  }
+}
+
